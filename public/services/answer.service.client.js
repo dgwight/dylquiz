a965e6149b1d455a45603cbd1fc24cc60044f4fe
Dylan Wight <dylangwight@gmail.com>
2017-05-28 13:08:30 -0400

    starting layout

---
+++ public/services/answer.service.client.js
@@ -0,0 +1,70 @@
+/**
+ * Created by DylanWight on 5/28/17.
+ */
+(function () {
+    angular
+        .module("WebAppMaker")
+        .factory("WidgetService", WidgetService);
+    function WidgetService() {
+        var widgets = [
+            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                "url": "http://lorempixel.com/400/200/"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        ];
+        var api = {
+            "createWidget": createWidget,
+            "findWidgetsByPageId": findWidgetsByPageId,
+            "findWidgetById": findWidgetById,
+            "updateWidget": updateWidget,
+            "deleteWidget": deleteWidget
+        };
+        return api;
+
+        function createWidget(pageId, widget) {
+            widget._id = widget._id ? widget._id : new Date().getTime() + "";
+            widget.pageId = pageId;
+            widgets.push(widget);
+            return widget;
+        }
+
+        function findWidgetsByPageId(pageId) {
+            var pageWidgets = [];
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i].pageId === pageId)
+                    pageWidgets.push(widgets[i]);
+            }
+            return pageWidgets;
+        }
+
+        function findWidgetById(widgetId) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id === widgetId)
+                    return widgets[i];
+            }
+            return null;
+        }
+
+        function updateWidget(widgetId, widget) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id === widgetId) {
+                    widgets[i] = widget;
+                    return widgets[i];
+                }
+            }
+        }
+
+        function deleteWidget(widgetId) {
+            for (var i = 0; i < widgets.length; i++) {
+                if (widgets[i]._id === widgetId) {
+                    widgets.splice(i, 1);
+                }
+            }
+        }
+    }
+})();
