e68715cbb743b38c438f0cb78c61916f1d2c8889
Dylan Wight <dylangwight@gmail.com>
2017-06-21 15:49:46 -0400

    getResult

--- api/router/recordRouter.js
+++ api/router/recordRouter.js
@@ -6,10 +6,11 @@ const CommonRouter = require('./commonRouter');
 function RecordRouter(app) {
     const RecordService = require("../services/RecordService")();
     const RecordRouter = new CommonRouter(app, RecordService, "record");
-    app.get('/api/record/:rid/next-question', nextQuestion);
+    app.get('/api/record/:rid/next-question', getNextQuestion);
+    app.get('/api/record/:rid/result', getResult);
     app.post('/api/record/:rid/answer-question', answerQuestion);
 
-    function nextQuestion(req, res) {
+    function getNextQuestion(req, res) {
         console.log(req.url, req.params);
         RecordService.getNextQuestion(req.params.rid)
             .then((err, doc) => RecordRouter.respond(err, doc, res));
@@ -20,6 +21,14 @@ function RecordRouter(app) {
         RecordService.answerQuestion(req.params.rid, req.body.answerId)
             .then((err, doc) => RecordRouter.respond(err, doc, res));
     }
+
+    function getResult(req, res) {
+        console.log(req.url, req.params, req.body);
+        RecordService.getResult(req.params.rid, req.body.answerId)
+            .then((err, doc) => RecordRouter.respond(err, doc, res));
+    }
+
+
 }
 
 module.exports = RecordRouter;
--- api/services/RecordService.js
+++ api/services/RecordService.js
@@ -6,6 +6,8 @@ const CommonService = require('./CommonService');
 const QuizService = require('./QuizService')();
 const QuestionService = require('./QuestionService')();
 const AnswerService = require('./AnswerService')();
+const ResultService = require('./ResultService')();
+
 
 function RecordService () {
     const RecordSchema = require("../schemas/recordSchema");
@@ -14,7 +16,6 @@ function RecordService () {
     RecordService.getNextQuestion = getNextQuestion;
     RecordService.answerQuestion = answerQuestion;
     RecordService.getResult = getResult;
-
 
     return RecordService;
 
@@ -38,26 +39,30 @@ function RecordService () {
         return AnswerService.findById(answerId).then((answer) => {
             this.answer = answer;
             return RecordService.add(id, answer._question, "questions");
-        // }).then((record) => {
-        //     return RecordService.add(id, this.answer._id, "answers");
         }).then((record) => {
-            // if (!record) return;
+            return RecordService.add(id, this.answer._id, "answers");
+        }).then((record) => {
+            if (!record) return;
             var update = {};
             update['$inc'] = {};
             for (var i = 0; i < this.answer.results.length; i++) {
                 update['$inc']["scores." + this.answer.results[i]] = this.answer.weight;
             }
-            console.log(update);
             return RecordService.model.findByIdAndUpdate(id, update, {strict: false, upsert: true, new: true});
         })
     }
 
     function getResult(id) {
         return RecordService.findById(id).then((record) => {
-            this.record = record;
-            return QuizService.findById(record._quiz);
-        }).then((quiz) => {
-            return QuestionService.findByQuizId(quiz._id);
+            var highestScore = 0;
+            var highestResult;
+            for (result in record.scores) {
+                if (highestScore <= record.scores[result]) {
+                    highestScore = record.scores[result];
+                    highestResult = result;
+                }
+             }
+            return ResultService.findById(highestResult);
         })
     }
 }
